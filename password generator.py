1ad9df5537b0b77f9204a128fa1e4439cd3476ca
Aleksandr Vetrov <aleksandrvetrov838@gmail.com>
2022-03-09 13:16:09 +0600

    Исправление ошибок и новые функции
    Исправлена структура репозитория.Исправлена ошибка с добавлением лишних пробелов от пользователя.Добавлена функция дозаписи паролей в файл. Небольшой рефакторинг.

--- password generator.py
+++ password generator.py
@@ -1,14 +1,30 @@
 from secrets import choice 
 
 def main():
-  length = int(input("Введите длинну пароля: "))
-  password = generate(length)
-  print(password)
-  print("Записать пароль в файл: y/n")
-  userAnswer = input()
-  if userAnswer.lower() == 'y':
-    with open('pass.txt', 'w') as file:
-      file.write(password)
+  generator()
+
+
+def generator():
+  flag = True
+  passwords = []
+  i = 0
+  while(flag):
+    length = int(input("Введите длинну пароля: "))
+    passwords.append(generate(length))
+    print(passwords[i])
+    i+=1
+    userAnswer = input("Создать новый пароль: y/n ")
+    if userAnswer.lower().strip() == 'n':
+      flag = False
+  userAnswer = input("Записать пароли в файл: y/n ")
+  if userAnswer.lower().strip() == 'y':
+    try:
+      with open('pass.txt', 'a') as file:
+        for i in passwords:
+          file.write(i + '\n')
+    except Exception:
+      with open('pass.txt', 'w') as file:
+        file.write('\n'.join(passwords) + '\n')
   else:
     print("Нажмите любую клавишу, чтобы выйти...")
     input()
@@ -24,9 +40,11 @@ def generate(length):
 
 
 def ascii():
-  result = []
-  for i in range(ord('0'), ord('~')+1):
-    result.append(chr(i))
-  return result
+  simbols = []
+  for i in range(ord('!'), ord('~')+1):
+    if i == 36 or i == 46 or i == 47 or i == 63:
+      continue
+    simbols.append(chr(i))
+  return simbols
 
 main()
