63750877b29a8bff4004854148000510d48c331f
Aleksandr Vetrov <aleksandrvetrov838@gmail.com>
2022-03-19 17:06:52 +0600

    Тестовая версия готова
    Пароли генерируются и выводятся на странице. Есть возможность выбрать количество символов и нежелательные для пароля символы.

--- server.py
+++ server.py
@@ -2,22 +2,19 @@ from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import request
+from flask import flash
+from flask import get_flashed_messages
 import passwordgenerator
 
 app = Flask("app")
+app.config['SECRET_KEY'] = "xOPd3OP4CHv)|^.UwEl2+GBSNglj~"
 
 
-@app.route('/', )
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
-
-
-@app.route('/password', methods=['POST', 'GET'])
-def password():
     if request.method == 'POST' and (request.form['password_length'] != ' ' and request.form['banned_symbols'] != ' '):
-        return passwordgenerator.generate(int(request.form['password_length']), request.form['banned_symbols'])
-    else:
-        return "Error"
+        flash(passwordgenerator.generate(int(request.form['password_length']), request.form['banned_symbols']))
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
