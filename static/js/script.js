f1f2334dd5d18a4e0ec47858a64f6a2008b97ccc
Aleksandr Vetrov <98459125+AlexPams@users.noreply.github.com>
2022-03-29 13:05:28 +0000

    Рабочая версия генератора
    Генератор готов к деплою. В будущем необходимо переписать из пост запроса в интерактивный режим. Сейчас генератор может сгенерировать пароль. С помощью чекбоксов удалить какие то символы. Редактировать длину пароля.

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,52 @@
-let ch1 = document.getElementById("checkbox1")
-let ch2 = document.getElementById("checkbox2")
-let ch3 = document.getElementById("checkbox3")
+const ch1 = document.getElementById("checkbox1");
+const ch2 = document.getElementById("checkbox2");
+const ch3 = document.getElementById("checkbox3");
+const symbols = document.getElementById("symbols");
+let letters = " a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+let digits = "1 2 3 4 5 6 7 8 9 0";
+let specialSymbols = "!  # $ % & ' ( ) * + , - _ [ ] / . ^ '\\' `  0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ { | } ~ ";
 
 function chbox1(params) {
   if (ch1.checked) {
-		document.getElementById("symbols").value = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if(symbols.value == "Введите нежелательный символы"){
+      symbols.value = letters;
+    }else{
+      symbols.value += letters;
+    }
+		
 	}
 	else {
-		document.getElementById("symbols").value = "Введите нежелательные символы"
+		symbols.value = symbols.value.replace(letters, "");
 	}
 }
+
+
 function chbox2(params) {
   if (ch2.checked) {
-		alert('Выбран');
+    if(symbols.value == "Введите нежелательный символы"){
+      symbols.value = specialSymbols;
+    }else{
+      symbols.value += specialSymbols;
+    }
+		
 	}
 	else {
-		alert ('Не выбран');
+		symbols.value = symbols.value.replace(specialSymbols, "");
 	}
 }
+
+
 function chbox3() {
   if (ch3.checked) {
-		alert('Выбран');
+    if(symbols.value == "Введите нежелательный символы"){
+      symbols.value = digits;
+    }else{
+      symbols.value += digits;
+    }
+		
 	}
 	else {
-		alert ('Не выбран');
+		symbols.value = symbols.value.replace(digits, "");
 	}
 }
 
